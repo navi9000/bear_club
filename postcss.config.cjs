367420c822cdd7090d883e53db874a4a46676251
navi9000 <icdi7434@gmail.com>
2025-10-29 15:17:32 +0300

    add sass

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("autoprefixer"),
-    require("postcss-nested"),
-    require("cssnano")({ preset: "default" }),
-  ],
-}
